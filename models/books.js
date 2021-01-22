334cd4cbf4f7e836e41802fb38b4593ed8a11f3d
hilekat <68864060+hilekat@users.noreply.github.com>
2021-01-22 09:18:19 -0600

    .js
    new update

--- models/books.js
+++ models/books.js
@@ -1,14 +1,30 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const db = require("../models");
 
-const bookSchema = new Schema({
-    title : String,
-    authors : [String],
-    description : String,
-    image : String,
-    link : String
-});
-
-const Book = mongoose.model("Book", bookSchema);
-
-module.exports = Book;
+module.exports = {
+    findAll: function(req, res) {
+      db.GoogleBooks
+        .find(req.query)
+        .sort({ date: -1 })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    findOne: function(req, res) {
+      db.GoogleBooks
+        .findOne({id: req.params.id})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    save: function(req, res) {
+      db.GoogleBooks
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+      db.GoogleBooks
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
+  };
