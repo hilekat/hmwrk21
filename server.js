a449cec900572996ffa148267cdf707fe3fdcb97
hilekat <68864060+hilekat@users.noreply.github.com>
2021-01-22 09:42:13 -0600

    pushing again
    pushing again

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
+// Define API routes here
 app.use(routes);
 
-
+// Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
 mongoose.connect(MONGODB_URI, {
